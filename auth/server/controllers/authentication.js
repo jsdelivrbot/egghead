74650d68fb868993e14f9f77bf11e9730a377a7d
Vince Sangkhae <vsangk@gmail.com>
2017-03-04 16:02:17 -0800

    Setup express and mongoose

---
+++ auth/server/controllers/authentication.js
@@ -0,0 +1,9 @@
+exports.signup = function(req, res, next) {
+  // See if a user with the given email exists
+
+  // If a user with email does exist, return an error
+
+  // If a user witih email does NOT exist, create and save user record
+
+  // Response to request indicating the user was created
+};
