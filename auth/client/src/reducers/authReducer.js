ea9e5fce43682282d0f859d6fb454febd19893d9
Vince Sangkhae <vsangk@gmail.com>
2017-03-07 11:08:32 -0800

    Implement signin logic

---
+++ auth/client/src/reducers/authReducer.js
@@ -0,0 +1,15 @@
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from '../actions/types';
+
+export default function(state = {}, action) {
+  switch (action.type) {
+    case AUTH_USER:
+      return Object.assign({}, state, { authenticated: true });
+    case UNAUTH_USER:
+      return Object.assign({}, state, { authenticated: false });
+    case AUTH_ERROR:
+      return Object.assign({}, state, { error: action.error });
+
+    default:
+      return state;
+  }
+}
