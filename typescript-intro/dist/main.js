7350f0acae71ffe317e15b6c88c1f7e2872e9b60
Vince Sangkhae <vsangk@gmail.com>
2017-04-08 22:32:03 -0700

    Add @types/es6-shim and rxjs library

--- typescript-intro/dist/main.js
+++ typescript-intro/dist/main.js
@@ -1,7 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var _ = require("lodash");
+require("rxjs/add/observable/timer");
+require("rxjs/add/observable/interval");
+// Observable.interval(1000)
+//   .subscribe(x => console.log(x));
 var App = (function () {
     function App() {
-        console.log('App working');
+        this.title = 'Eggheads';
     }
+    App.prototype.getUsers = function () {
+        return [{ name: 'Vince' }];
+    };
     return App;
 }());
 var app = new App();
+console.log(_.isArray(new App().getUsers()));
--- typescript-intro/src/main.ts
+++ typescript-intro/src/main.ts
@@ -1,7 +1,21 @@
-class App {
-  constructor() {
-    console.log('App working');
+import * as _ from 'lodash';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/observable/interval';
+
+import { SocialNetwork } from './social-network';
+
+// Observable.interval(1000)
+//   .subscribe(x => console.log(x));
+
+class App implements SocialNetwork {
+  title = 'Eggheads';
+
+  getUsers() {
+    return [{ name: 'Vince' }];
   }
 }
 
 const app = new App();
+
+console.log(_.isArray(new App().getUsers()));
