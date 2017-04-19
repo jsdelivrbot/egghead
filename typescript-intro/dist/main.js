370cc9359fe75860782e2fc2c30d9b87cf17fea6
Vince Sangkhae <vsangk@gmail.com>
2017-04-19 09:06:03 -0700

    Add ts interface and decorators

--- typescript-intro/dist/main.js
+++ typescript-intro/dist/main.js
@@ -1,10 +1,15 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var _ = require("lodash");
+var Observable_1 = require("rxjs/Observable");
 require("rxjs/add/observable/timer");
 require("rxjs/add/observable/interval");
-// Observable.interval(1000)
-//   .subscribe(x => console.log(x));
 var App = (function () {
     function App() {
         this.title = 'Eggheads';
@@ -16,3 +21,42 @@ var App = (function () {
 }());
 var app = new App();
 console.log(_.isArray(new App().getUsers()));
+Observable_1.Observable.timer(1000)
+    .subscribe(function (x) { return console.log('Timer Done!'); });
+function addAge(age) {
+}
+// // Old way of doing decorators
+// const person = { name: 'Vince' };
+//
+// function addAge(age) {
+//   return function(person) {
+//     return {
+//       age,
+//       name: person.name
+//     }
+//   }
+// }
+//
+// const newPerson = addAge(26)(person);
+// console.log(newPerson);
+function addAge(age) {
+    return function (targetClass) {
+        return (function () {
+            function class_1() {
+                this.name = new targetClass().name;
+                this.age = age;
+            }
+            return class_1;
+        }());
+    };
+}
+var Person = (function () {
+    function Person() {
+        this.name = 'Vince';
+    }
+    return Person;
+}());
+Person = __decorate([
+    addAge(26)
+], Person);
+console.log(new Person());
--- typescript-intro/src/main.ts
+++ typescript-intro/src/main.ts
@@ -5,9 +5,6 @@ import 'rxjs/add/observable/interval';
 
 import { SocialNetwork } from './social-network';
 
-// Observable.interval(1000)
-//   .subscribe(x => console.log(x));
-
 class App implements SocialNetwork {
   title = 'Eggheads';
 
@@ -19,3 +16,40 @@ class App implements SocialNetwork {
 const app = new App();
 
 console.log(_.isArray(new App().getUsers()));
+
+Observable.timer(1000)
+  .subscribe(x => console.log('Timer Done!'));
+
+function addAge(age) {
+}
+
+// // Old way of doing decorators
+// const person = { name: 'Vince' };
+//
+// function addAge(age) {
+//   return function(person) {
+//     return {
+//       age,
+//       name: person.name
+//     }
+//   }
+// }
+//
+// const newPerson = addAge(26)(person);
+// console.log(newPerson);
+
+function addAge(age) {
+  return function(targetClass) {
+    return class {
+      name = new targetClass().name;
+      age = age;
+    }
+  }
+}
+
+@addAge(26)
+class Person {
+  name = 'Vince';
+}
+
+console.log(new Person());
